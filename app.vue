65a89fe1f6b707b9a9e24d1afeed87ade7e62070
rastridge <rfa@me.com>
2024-07-05 14:31:06 -0400

    first commit on main

---
+++ app.vue
@@ -0,0 +1,49 @@
+<template>
+  <NuxtLayout>
+    <NuxtLoadingIndicator :height="10" />
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+  import { useMenuStore } from '~/stores/menuStore'
+  const menuStore = useMenuStore()
+
+  useHead({
+    title: 'The Buffalo Rugby Club',
+  })
+
+  useSeoMeta({
+    title: 'The Buffalo Rugby Clubx',
+    // url: 'https://experimental--buffalorugbyorg.netlify.app/archives/news/html/1067',
+
+    ogTitle: 'The Buffalo Rugby Cluby',
+    description: 'News',
+    ogDescription: 'News',
+    // ogUrl:
+    //   'https://experimental--buffalorugbyorg.netlify.app/archives/news/html/1067',
+    ogImage: 'https://experimental--buffalorugbyorg.netlify.app//bflo_blue.png',
+  })
+
+  /*   useSeoMeta({
+      title: 'The Buffalo Rugby Club',
+      ogUrl: 'https://buffalorugby.org/archives/news/page/1068',
+
+      ogType: 'article',
+      ogTitle: 'When Great Minds Don’t Think Alike',
+      ogDescription: 'Buffalo Rugby Club News',
+      ogImage: 'https://buffalorugby.org/bflo_blue.png',
+    }) */
+  //
+  // get custom menu items
+  await menuStore.initCustomMenuItems()
+
+  // stay logged in?
+  /* 	if (localStorage.getItem('auth')) {
+      	auth.loginAuto()
+      } */
+</script>
+
+<style lang="scss">
+  @use '~/assets/sass/layout/layout';
+</style>
---
+++ pages/testpage.vue
@@ -0,0 +1,20 @@
+<script setup>
+const originaltitle = ref('from testpage')
+const title = ref(originaltitle.value)
+
+			console.log(title.value)
+</script>
+
+<template>
+	<div>
+		<p>Go there Go there Go there Go there</p>
+		<!-- <testvmodel :modelValue="title" @update:modelValue="$event => (title = $event) " /> -->
+		<testvmodel v-model="title" />
+		<br>
+		in testpage originaltitle = {{ originaltitle }}
+		<br>
+		in testpage title = {{ title }}
+		<br>
+		<br>
+	</div>
+</template>
