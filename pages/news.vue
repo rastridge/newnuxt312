b74709d347846d87b87bafbd45637df21088513a
rastridge <rfa@me.com>
2024-08-03 16:48:40 -0400

    testing branch

--- pages/news.vue
+++ pages/news.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Head>
-      <Title>Buffalo Rugby Club News</Title>
+      <Title>Buffalo Rugby Club New</Title>
     </Head>
     <Common-header title="Current News" />
     <div class="my-simple-card-style">
