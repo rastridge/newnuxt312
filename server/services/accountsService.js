b8754ce208949d0359cc760860bb3b324434235b
rastridge <rfa@me.com>
2024-08-07 18:46:43 -0400

    multiople emails

--- server/services/accountsService.js
+++ server/services/accountsService.js
@@ -241,7 +241,7 @@ async function addOne(info) {
 
       console.log('email_list = ', email_list)
 
-      /*       for (const email of email_list) {
+      for (const email of email_list) {
         console.log('email = ', email)
         await sendEmail(
           // CONFIG.TO,
@@ -249,13 +249,13 @@ async function addOne(info) {
           'Buffalo Rugby Club Member Account Creation',
           email_msg,
         )
-      } */
+      }
 
-      await sendEmail(
+      /*       await sendEmail(
         CONFIG.TO,
         'Buffalo Rugby Club Member Account Creation',
         email_msg,
-      )
+      ) */
     }
 
     await CONN.query('COMMIT')
