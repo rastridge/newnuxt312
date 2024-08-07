6e0bbaaa9598a457b745f0710f29604a8860a249
rastridge <rfa@me.com>
2024-08-07 15:51:29 -0400

    update registration response

--- pages/registered.vue
+++ pages/registered.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <p>Thanks for registering</p>
+    <h3>Thanks for registering</h3>
+    <p>Now its time to come to a practice</p>
+    <p>Wear shorts, cleats and a t-shirt ( or warmer if necessary)</p>
+    <p>
+      When youu arrive, ask someone to point out a coach, introduce yourself
+    </p>
+    <p>Do the warmup</p>
+    <p>Watch a drill or activity and join in when comfortable</p>
+    <p>
+      If you enjoy the challenge, ask the coach to change your membership status
+      to "Active"
+    </p>
+    <p>Ask what you need to do to become a full member.</p>
   </div>
 </template>
 <script setup></script>
