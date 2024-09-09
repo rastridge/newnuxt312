67c80adef8075504ef96cd648dc6f5f9b995d632
rastridge <rfa@me.com>
2024-09-09 13:10:15 -0400

    update rugby xplorer links

--- pages/otherresults.vue
+++ pages/otherresults.vue
@@ -1,34 +1,39 @@
 <template>
-	<div>
-		<Head>
-			<Title>Other EGU Results and Standingst</Title>
-		</Head>
-		<div class="topsectioncenter">
-			<div class="topsectionitem">
-				<Common-header title="Empire Geographical Union" />
-			</div>
+  <div>
+    <Head>
+      <Title>Other EGU Results and Standingst</Title>
+    </Head>
+    <div class="topsectioncenter">
+      <div class="topsectionitem">
+        <Common-header title="Empire Geographical Union" />
+      </div>
 
-			<div class="topsectionitem">
-				<Card>
-					<template #content>
-						<nuxt-link
-							to="https://xplorer.rugby/empire/fixtures-results?comp=F55b2bWvN7CKLA6cE&team=All&tab=Results"
-							target="_blank"
-							rel="noopener"
-						>
-							<Button label="League Results" class="m-1"></Button>
-						</nuxt-link>
-
-						<nuxt-link
-							to="https://xplorer.rugby/empire/fixtures-results?comp=F55b2bWvN7CKLA6cE&team=All&tab=Ladder"
-							target="_blank"
-							rel="noopener"
-						>
-							<Button label="League Standings" class="m-1" />
-						</nuxt-link>
-					</template>
-				</Card>
-			</div>
-		</div>
-	</div>
+      <div class="topsectionitem">
+        <Card>
+          <template #content>
+            <nuxt-link
+              to="https://xplorer.rugby/empire/fixtures-results?comp=7i8kPXDpmeAyzH2Mu&team=All&season=All&tab=Results"
+              target="_blank"
+              rel="noopener"
+            >
+              <Button
+                label="D2 League Results"
+                class="m-1"
+              ></Button>
+            </nuxt-link>
+            <nuxt-link
+              to="https://xplorer.rugby/empire/fixtures-results?comp=7i8kPXDpmeAyzH2Mu&team=All&season=All&tab=Results"
+              target="_blank"
+              rel="noopener"
+            >
+              <Button
+                label="D3 North League Results"
+                class="m-1"
+              ></Button>
+            </nuxt-link>
+          </template>
+        </Card>
+      </div>
+    </div>
+  </div>
 </template>
