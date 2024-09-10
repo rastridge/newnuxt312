955627f90e354fc14afb76e3f870ae20ab4a3763
rastridge <rfa@me.com>
2024-09-09 21:19:11 -0400

    use iframe

--- pages/otherresults.vue
+++ pages/otherresults.vue
@@ -5,12 +5,23 @@
     </Head>
     <div class="topsectioncenter">
       <div class="topsectionitem">
-        <Common-header title="Empire Geographical Union" />
+        <Common-header title="Empire Geographical Union Stats" />
       </div>
+    </div>
+    <div>
+      <Card>
+        <template #content>
+          <h3 class="my-header-style">Empire D2 League</h3>
+          <iframe
+            src="https://xplorer.rugby/empire/fixtures-results?comp=7i8kPXDpmeAyzH2Mu&team=All&season=2025&tab=Fixtures"
+            width="100%"
+            height="480"
+            style="border: 1px solid #eee; background: white"
+            frameborder="0"
+            scrolling="yes"
+          ></iframe>
 
-      <div class="topsectionitem">
-        <Card>
-          <template #content>
+          <!--
             <nuxt-link
               to="https://xplorer.rugby/empire/fixtures-results?comp=7i8kPXDpmeAyzH2Mu&team=All&season=2025&tab=Fixtures"
               target="_blank"
@@ -21,6 +32,23 @@
                 class="m-1"
               ></Button>
             </nuxt-link>
+            -->
+        </template>
+      </Card>
+      <div>
+        <Card>
+          <template #content>
+            <h3 class="my-header-style">Empire D3 Upstate League</h3>
+            <iframe
+              src="https://xplorer.rugby/empire/fixtures-results?comp=9LmeCHHfxEnJCEmap&team=All&season=2025&tab=Fixtures"
+              width="100%"
+              height="480"
+              style="border: 1px solid #eee; background: white"
+              frameborder="0"
+              scrolling="yes"
+            ></iframe>
+
+            <!--
             <nuxt-link
               to="https://xplorer.rugby/empire/fixtures-results?comp=9LmeCHHfxEnJCEmap&team=All&season=2025&tab=Fixtures"
               target="_blank"
@@ -31,6 +59,7 @@
                 class="m-1"
               ></Button>
             </nuxt-link>
+            -->
           </template>
         </Card>
       </div>
